5fcb331775417ecb4487d1c5596f1c5a313ed39b
CGAJAY <jmugendi737@gmail.com>
2024-11-09 16:25:51 +0300

    Styled the app

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ const App = () => {
 	const handleClick = () =>
 		setCount((prevCount) => prevCount + 1);
 	return (
-		<div className="container">
-			<h1>Welcome to the User App</h1>
-			<button onClick={handleClick}>Increment Count</button>
-			<p className="count">Count: {count}</p>
+		<div className="container flex justify-center flex-col items-center">
+			<h1 className="text-2xl font-extrabold my-4">
+				Welcome to the User App
+			</h1>
+			<button
+				className="bg-blue-300 p-2 shadow shadow-slate-500"
+				onClick={handleClick}
+			>
+				Increment Count
+			</button>
+			<p className="my-2">Count: {count}</p>
 			<UserCard key={user.id} name={user.name} />
 		</div>
 	);
