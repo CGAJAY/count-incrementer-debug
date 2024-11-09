958029f123c8845c87403a20b8d1f833c0e4f34c
CGAJAY <jmugendi737@gmail.com>
2024-11-09 16:12:40 +0300

    set up state to hold count, function to increase count

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { useState } from "react";
+import UserCard from "./components/UserCard";
 const App = () => {
-	return <div>Hello world!</div>;
+	const [count, setCount] = useState(0);
+	const user = { id: 1, name: "John Doe" };
+
+	const handleClick = () =>
+		setCount((prevCount) => prevCount + 1);
+	return (
+		<div className="container">
+			<h1>Welcome to the User App</h1>
+			<button onClick={handleClick}>Increment Count</button>
+			<p className="count">Count: {count}</p>
+			<UserCard key={user.id} name={user.name} />
+		</div>
+	);
 };
 
 export default App;
